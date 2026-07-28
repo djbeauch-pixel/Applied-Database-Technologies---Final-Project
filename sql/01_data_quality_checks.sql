41084fb5965520a650f07e649cdccda8ea7b42e2
djbeauch-pixel <djbeauch@iu.edu>
2026-07-28 18:00:32 -0400

    Update 01_data_quality_checks.sql

--- sql/01_data_quality_checks.sql
+++ sql/01_data_quality_checks.sql
@@ -1,6 +1,9 @@
-# Reviewing Numbers of data inserted into database
+--Reviewing Numbers of data inserted into database across each table
 SELECT COUNT(*) AS hands FROM hands;
 SELECT COUNT(*) AS players FROM players;
 SELECT COUNT(*) AS seats FROM hand_players;
 SELECT COUNT(*) AS actions FROM actions;
-SELECT stake, COUNT(*) AS hands FROM hands GROUP BY stake;
+
+SELECT stake, COUNT(*) AS hands --How many hands per stake?
+  FROM hands 
+  GROUP BY stake;
